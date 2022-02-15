bab5de77360a5dccbba536a075c738fe5e69ebeb
Saraediani <sara.pur187@gmail.com>
2022-02-15 15:41:55 +0100

    react

---
+++ express/server/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import 'dotenv/config';
+import Router from '../router/index.js';
+import path from 'path';
+
+
+
+class Server {
+    constructor() {
+        this.router = Router;
+        this.port = process.env.PORT;
+        this.app = express();
+    }
+
+    start() {
+        this._setupRoutes();
+        this._listen();
+
+    }
+
+    _setupRoutes() {
+        this.router.create(this.app);
+    }
+
+    _listen() {
+        this.app.listen(this.port, () => {
+            console.log(`App is running on port ${this.port}`);
+        });
+    }
+
+
+
+
+}
+
+export default Server;
