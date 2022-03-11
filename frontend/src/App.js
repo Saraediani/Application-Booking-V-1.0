8077ceb3eb180d314167f5628f13d8a7c9f77ad2
Elhilali-hA <Elhilali-hA>
2022-03-11 16:39:43 +0100

    dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,23 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import Main from './components/Main';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import Navbar from './components/Navbar/Navbar';
+
 function App() {
   const user = localStorage.getItem("token") 
   return (
+
+    
+
     <Routes>
-       {user && <Route path="/"  element={<Main />} />}
+       {user && <Route path="/"  element={<Main />} />}  
+      
        <Route path="/signup" element={<Signup />} />
+       <Route path="/home" element={<Navbar />} />
        <Route path="/login" element={<Login />}/>
        <Route path="/" element={<Navigate replace to="/login" />}/>
     </Routes>
+    
   );
 }
 export default App;  
---
+++ frontend/src/components/Navbar/SidebarData.jsx
@@ -0,0 +1,34 @@
+export const SidebarData = [
+  {
+    title: 'dashboard',
+    path: '/',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Reports',
+    path: '/reports',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Products',
+    path: '/products',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Team',
+    path: '/team',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Messages',
+    path: '/messages',
+    
+    cName: 'nav-text'
+  },
+  {
+    title: 'Support',
+    path: '/support',
+   
+    cName: 'nav-text'
+  }
+];
