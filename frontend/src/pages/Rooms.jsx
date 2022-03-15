13a2520cf0023f6993310dd085e2797e8b15d059
Elhilali-hA <Elhilali-hA>
2022-03-15 16:23:03 +0100

    comm_to_merge

--- frontend/src/pages/Rooms.jsx
+++ frontend/src/pages/Rooms.jsx
@@ -3,9 +3,12 @@ import Hero from '../components/Hero'
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import RoomsContainer from '../components/RoomsContainer';
+import Navbar from '../components/Navbar'
+
 const Rooms = () => {
     return (
     <div>
+         <Navbar />
         <Hero hero="roomsHero">
         </Hero>
         <Banner title="Available Rooms" subtitle="Best in Class Room">
